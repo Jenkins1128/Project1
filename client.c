a1413dd0390c7d6b9dcdeb22d335e48c3c211d08
Malik <iamanomalik@gmail.com>
2020-04-03 15:25:50 -0700

    cli cleanup

--- client.c
+++ client.c
@@ -13,149 +13,129 @@
 
 #define DST_PORT 8765 
 #define TCP_PORT 8080
-#define DST_IP "192.168.56.101"
+#define DST_IP   "192.168.56.101"
 
-#define MAXLINE 6000	
-#define MAX 80
+#define MAXLINE  6000	
+#define MAX 	 80
   
 #define PAYLOAD_SIZE 1002
 #define SA struct sockaddr 
 
-void func(int sockfd) 
-{ 
-    char buff[MAX]; 
-    int n; 
-    for (;;) { 
-        bzero(buff, sizeof(buff)); 
-        printf("Enter the string : "); 
-        n = 0; 
-        while ((buff[n++] = getchar()) != '\n') 
-            ; 
-        write(sockfd, buff, sizeof(buff)); 
-        bzero(buff, sizeof(buff)); 
-        read(sockfd, buff, sizeof(buff)); 
-        printf("From Server : %s", buff); 
-        if ((strncmp(buff, "exit", 4)) == 0) { 
-            printf("Client Exit...\n"); 
-            break; 
-		}
-    } 
-} 
-
-void recvFile(int sockfd) 
-{ 
-	char buff[MAX]; 	// to store message from client
+/* Function used to receive file */
+void recvFile(int sockfd) { 
+	// Buffer used to receive file 
+	char buff[MAX]; 
 	
+	// Opening file that that will hold received file
 	FILE *fp;
-	fp=fopen("received.c","w"); // stores the file content in recieved.txt in the program directory
 	
-	if( fp == NULL ){
-		printf("Error IN Opening File ");
+	// Ensure the file 
+	if((fp = fopen("received.c", "w")) == NULL) {
+		perror("Error IN Opening File");
 		return ;
 	}
 	
-	while( read(sockfd,buff,MAX) > 0 )
-		fprintf(fp,"%s",buff);
+	// Read the file and write it to file
+	while(read(sockfd, buff, MAX) > 0) {
+		fprintf(fp, "%s", buff);
+	}
 	
+	// Close file pointer after writing to the file
 	fclose(fp);
-	printf("File received successfully !! \n");
-	printf("New File created is received.txt !! \n");
-
 } 
 
-void sentFile(int sockfd) 
-{ 
-	char buff[MAX]; 						// for read operation from file and used to sent operation 
+/* Function used to send file */
+void sentFile(int sockfd) { 
+	// Buffer used to receive file 
+	char buff[MAX];
 	
-	// create file 
+	// Create file 
 	FILE *fp;
-	fp=fopen("client.c","r");		// open file uses both stdio and stdin header files
-											// file should be present at the program directory
 
-	if( fp == NULL ){
+	// Open file and ensure it opens
+	if((fp=fopen("client.c", "r")) == NULL) {
 		printf("Error IN Opening File .. \n");
 		return ;
 	}
 	
-	while ( fgets(buff,MAX,fp) != NULL )	// fgets reads upto MAX character or EOF 
-		write(sockfd,buff,sizeof(buff)); 	// sent the file data to stream
-	
-	fclose (fp);							// close the file 
-	
-	printf("File Sent successfully !!! \n");
-	
-} 
-
-void pre_probe_cli() {
-	int sockfd, connfd; 
-	struct sockaddr_in servaddr, cli; 
-
-	// socket create and varification 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-	if (sockfd == -1) { 
-		printf("socket creation failed...\n"); 
-		exit(0); 
-	} else {
-		printf("Socket successfully created..\n"); 
+	// Read the contents of the file into the buffer and send it over socket
+	while (fgets(buff, MAX, fp) != NULL) {
+		write(sockfd, buff, sizeof(buff));
 	}
 	
+	// Close file pointer after writing to the file
+	fclose(fp);					
+} 
+
+/* Function used for handling pre-phobing phase */
+void pre_probe_cli() {
+	int sockfd; 
+	struct sockaddr_in servaddr; 
+
+	// Create TCP socket and make sure it was created successfully
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
+		perror("socket creation failed..."); 
+        exit(EXIT_FAILURE); 
+	}
+	
+	// Allow the socket to reuse a socket in use (needed for consecutive runs)
 	int on = IP_PMTUDISC_DO;
 	int sso_return = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
 	bzero(&servaddr, sizeof(servaddr)); 
 
-	// assign IP, PORT 
+	// Assign IP, PORT for server information
 	servaddr.sin_family = AF_INET; 
 	servaddr.sin_addr.s_addr = inet_addr(DST_IP); 
 	servaddr.sin_port = htons(TCP_PORT); 
 
-	// connect the client socket to server socket 
+	// Connect client socket to server socket 
 	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
-		printf("connection with the server failed...\n"); 
-		exit(0); 
+		perror("connection with the server failed..."); 
+        exit(EXIT_FAILURE); 
 	} 
-	else
-		printf("connected to the server..\n"); 
 
-	// function for sending File 
+	// Fnction for sending File 
 	sentFile(sockfd); 
 
-	// close the socket 
-	int return_value = close(sockfd); 
-	printf("return:value:%d\n", return_value);
+	// Close the socket 
+	close(sockfd); 
 }
 
 void probe_cli() {
-    int sockfd; 
-	int rcvd_msg;
+    int sockfd, rcvd_msg; 
     char buffer[MAXLINE]; 
-    char *hello = "Hello from client"; 
     struct sockaddr_in servaddr, cliaddr; 
 
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+    // Create UDP socket and make sure it was created succesfully
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) { 
         perror("socket creation failed"); 
         exit(EXIT_FAILURE); 
     } 
+	
+	// Set socket to have DON'T FRAGMENT bit set
 	int on = IP_PMTUDISC_DO;
 	setsockopt(sockfd, IPPROTO_IP, IP_MTU_DISCOVER, &on, sizeof(on));
   
-    // Server information 
+	// Assign IP, PORT for server information
     memset(&servaddr, 0, sizeof(servaddr)); 
     servaddr.sin_family = AF_INET; 
     servaddr.sin_port = htons(DST_PORT); 
     servaddr.sin_addr.s_addr = inet_addr(DST_IP); 
 
-	// Client information
+	// Assign IP, PORT for client information (needed for making the right port)
 	memset(&cliaddr, 0, sizeof(cliaddr));
     cliaddr.sin_family = AF_INET; 
     cliaddr.sin_port = htons(SRC_PORT); 
     cliaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
+	// Needed to make sure it shows right source port
 	if (bind(sockfd, (struct sockaddr *) &cliaddr, sizeof(cliaddr)) < 0) {
 		perror("bind error");
 		exit(EXIT_FAILURE);
 	}
 
+	// Needed for random seed
 	srand(time(0));	
 
 	// Creating Packet Train
@@ -176,43 +156,35 @@ void probe_cli() {
 				// packet_train[i][j] = hold_rand_num[0];
 				packet_train[i][j] = '0'; // All 0's in
 			}
-			packet_train[i][PAYLOAD_SIZE - 2] = '0'; // All 0's in
+			packet_train[i][PAYLOAD_SIZE - 2] = '\0'; // All 0's in
 		}
 	}
 
-	// Message to server to start timer
+	// Send message to server to indicate next packet will be start of Low Entrophy
+	// packet train. Let's server know to start timer
+	// Ensures the server gets the message. 
 	char* start_msg = "Start LOW UDP Train";
-	int confirm_received = 0;
 	int len = sizeof(servaddr);
-	while (confirm_received == 0) {
-		printf("Running START\n");
+	while (strcmp(start_msg, buffer) == 0) {
 		sendto(sockfd, start_msg, strlen(start_msg), 
 			MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
 				sizeof(servaddr)); 
 		rcvd_msg = recvfrom(sockfd, (char *)buffer, MAXLINE,
 							MSG_DONTWAIT, ( struct sockaddr *)&servaddr,
 							&len);
-		printf("RCVFROM: %s\n", buffer);
-		if (strcmp(start_msg, buffer) == 0) {
-			printf("Message Received\n");
-			confirm_received = 1;
-		}
 	}
 
 	// Sending Packet Train
 	for (int i = 0; i < sizeof(packet_train)/sizeof(packet_train[0]); i++) {
-	// for (int i = 0; i < 6000; i++) {
 		sendto(sockfd, (const char *)packet_train[i], strlen(packet_train[i]), 
 			MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
 				sizeof(servaddr)); 
 	}
 
-	// Message to end to server to stop recording time
+	// Message to send to server to stop recording time
 	char end_msg[100] = "End LOW UDP Train";
-	printf("%s\n", end_msg);
-
-	confirm_received = 0;
-	while (confirm_received == 0) {
+	// Ensures the server gets the message. 
+	while (strcmp(start_msg, buffer) == 0) {
 		printf("Running END\n");
 		sendto(sockfd, (const char *)end_msg, strlen(end_msg), 
 			MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
@@ -220,62 +192,45 @@ void probe_cli() {
 		rcvd_msg = recvfrom(sockfd, (char *)buffer, MAXLINE,
 							MSG_DONTWAIT, ( struct sockaddr *)&servaddr,
 							&len);
-		if (strcmp(start_msg, buffer) == 0) {
-			printf("Message Received\n");
-			confirm_received = 1;
-		}
 	}
-  
-    int close_return = close(sockfd); 
-	printf("close_return: %d\n", close_return);
+
+	// Close socket when done
+    close(sockfd); 
 }
 
 void post_probe_cli() {
-    int sockfd, connfd; 
-    struct sockaddr_in servaddr, cli; 
-  
-    // socket create and varification 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) { 
-        printf("socket creation failed...\n"); 
-        exit(0); 
-    } else {
-        printf("Socket successfully created..\n"); 
-	}
+	int sockfd; 
+	struct sockaddr_in servaddr; 
 
+	// Create TCP socket and make sure it was created successfully
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
+		perror("socket creation failed..."); 
+        exit(EXIT_FAILURE); 
+	}
+	
+	// Allow the socket to reuse a socket in use (needed for consecutive runs)
 	int on = IP_PMTUDISC_DO;
 	int sso_return = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
-	printf("SSORETURN: %d\n", sso_return);
-	
-    bzero(&servaddr, sizeof(servaddr)); 
-  
-    // assign IP, PORT 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = inet_addr(DST_IP); 
-    servaddr.sin_port = htons(8070); 
-  
-    // connect the client socket to server socket 
-    while (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
-        printf("connection with the server failed...\n"); 
-	}
-	/*
-    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
-        perror("connection with the server failed..."); 
-        exit(0); 
-    } else {
-        printf("connected to the server..\n"); 
-	}
-	*/
-  
-    // function for chat 
-    // func(sockfd); 
+	bzero(&servaddr, sizeof(servaddr)); 
 
+	// Assign IP, PORT for server information
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = inet_addr(DST_IP); 
+	servaddr.sin_port = htons(TCP_PORT); 
+
+	// Keep attempting a connection to the server until one is achieved.
+    while (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
+        perror("connection with the server failed..."); 
+	}
+
+	// Buffer to receive message from server on its findings
     char buff[MAX]; 
 	bzero(buff, sizeof(buff)); 
 	read(sockfd, buff, sizeof(buff)); 
-	printf("From Server : %s\n", buff); 
+	printf("Server Findings: %s\n", buff); 
   
-    // close the socket 
+    // Close the socket when done
     close(sockfd); 
 }
 
